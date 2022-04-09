15015a912f48915fa09b25f7501594340cd99cd6
JackFishburger <wenxuan1220@gmail.com>
2022-04-09 16:53:30 -0400

    figured out how to interact with the api and return resturant in range

--- api/api.py
+++ api/api.py
@@ -1,29 +1,28 @@
-import requests
-from urllib.parse import urlencode
+import api_interactions as ai
+import parsing_lst as par
 
-with open('apikey.txt') as file:
-    key = file.readlines()
-file.close()
+near_by_resturants = ai.search_nearby('20742', 5000, 'chinese')
+print('----------places with name and their place_id----------')
+print(near_by_resturants)
+print('--------------------------------')
+final_result = []
 
-api_key = key[0]
+for tuple in near_by_resturants:
+    place_id = tuple[1]
+    address_info_lst = ai.place_id_to_address_lst(place_id)
+    final_address = par.info_lst_to_address(address_info_lst)
+    temp = (tuple[0], final_address)
+    final_result.append(temp)
 
-# address could be both address or postalcode
-def extract_lat_lng(address_or_postalcode):
-    data_type = 'json'
-    endpoint = f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
-    params = {"address": address_or_postalcode, "key": api_key}
-    url_params = urlencode(params)
-    url = f"{endpoint}?{url_params}"
-    r = requests.get(url)
-    if r.status_code not in range(200, 299): 
-        return {}
-    latlng = {}
-    try:
-        latlng = r.json()['results'][0]['geometry']['location']
-    except:
-        pass
-    return latlng.get("lat"), latlng.get("lng")
+print(final_result)
 
-print(extract_lat_lng('20742'))
+
+
+
+
+
+
+
+
 
 
