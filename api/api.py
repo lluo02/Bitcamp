d327a7c27cd42bb32f7952bfaad891c558471fd4
JackFishburger <wenxuan1220@gmail.com>
2022-04-09 17:55:22 -0400

    fixed minor issue

--- api/api.py
+++ api/api.py
@@ -1,20 +1,24 @@
 import api_interactions as ai
 import parsing_lst as par
 
-near_by_resturants = ai.search_nearby('20742', 5000, 'chinese')
-print('----------places with name and their place_id----------')
-print(near_by_resturants)
-print('--------------------------------')
-final_result = []
+def get_near_by_resturants(address_or_postalcode, radius, type_of_food):
+    near_by_resturants = ai.search_nearby(address_or_postalcode, radius, type_of_food)
+    final_result = []
+    for tuple in near_by_resturants:
+        place_id = tuple[1]
+        address_info_lst = ai.place_id_to_address_lst(place_id)
+        final_address = par.info_lst_to_address(address_info_lst)
+        temp = (tuple[0], final_address)
+        final_result.append(temp)
 
-for tuple in near_by_resturants:
-    place_id = tuple[1]
-    address_info_lst = ai.place_id_to_address_lst(place_id)
-    final_address = par.info_lst_to_address(address_info_lst)
-    temp = (tuple[0], final_address)
-    final_result.append(temp)
+    return final_result
 
-print(final_result)
+print('----------address 20740----------')
+print('----------near by chinese food with in 1000 meter----------')
+print(get_near_by_resturants('20740', 1000, 'chinese'))
+print('----------near by mexican food with in 1000 meter----------')
+print(get_near_by_resturants('20740', 1000, 'mexican'))
+
 
 
 
